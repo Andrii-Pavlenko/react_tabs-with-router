0ef8878bcb677a409ac431672e328b1344f7bbd8
Andrii <farikpite@gmail.com>
2019-11-06 22:25:46 +0200

    Updates

---
+++ precache-manifest.d0128dbaaa7cbb1041ed2c4632543b72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef920dbd0ec8d877408eba19f1201ec",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "97f0c383b24b1f8c64f6",
+    "url": "/react_tabs-with-router/static/css/main.8d49ef96.chunk.css"
+  },
+  {
+    "revision": "4bf3f87d43bd9fabcc07",
+    "url": "/react_tabs-with-router/static/js/2.22479ee1.chunk.js"
+  },
+  {
+    "revision": "97f0c383b24b1f8c64f6",
+    "url": "/react_tabs-with-router/static/js/main.644f058d.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
